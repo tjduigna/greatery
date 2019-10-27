1e6cdcd8eb36cb497026f207bcf81c7b8ca64b12
tjduigna <tjduigna@buffalo.edu>
2019-10-27 17:36:04 -0400

    Work on deployment

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 # assumes already logged in to docker hub
-sudo docker build -t greatery-base:latest -f Dockerfile.base .
-sudo docker build -t greatery:latest -f Dockerfile.greatery .
+#sudo docker build -t greatery:base-latest -f Dockerfile.base .
+sudo docker build -t 597104984438.dkr.ecr.us-east-1.amazonaws.com/greatery:latest -f Dockerfile.greatery .
+
+# sudo docker image inspect greatery:latest > manifest.json
+
+# assumes aws configure already done
+aws ecr get-login
+sudo docker `aws ecr get-login` # but remove the -e none ? unset param..
