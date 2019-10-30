7ed6d3e7958fca5654c5f00c2451b2aabc9718a5
tjduigna <tjduigna@buffalo.edu>
2019-10-29 23:39:19 -0400

    round trip to front-end 'auth'

--- greatery/api/auth.py
+++ greatery/api/auth.py
@@ -13,7 +13,6 @@ import greatery
 class LoginHandler(RequestHandler):
 
     def set_default_headers(self):
-        print("setting headers")
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "*")
         self.set_header("Access-Control-Request-Headers",
@@ -26,12 +25,28 @@ class LoginHandler(RequestHandler):
 
     async def post(self):
         req = json.loads(self.request.body)
-        for key in ['username', 'password']:
-            assert key in req
-        print(req)
-
+        #print("json loaded request")
+        user = req.get('username', None)
+        pw = req.get('password', None)
+        #print(req)
+        #print(user, pw)
+        if not user or not pw:
+            #print("request fkd")
+            self.write({"error": "malformed request"})
+            self.set_status(407)
+            self.finish()
+            return
+        if user != 'greatery-admin' or pw != 'supersecret':
+            #print("request unauthorized")
+            self.write({"error": "authentication denied"})
+            self.set_status(403)
+            self.finish()
+            return
+        #print("Made it past all the checks")
+        self.write(json.dumps({"authorized": True}))
+        self.set_status(200)
 
     async def options(self):
-        print("options", self.request)
+        #print("options", self.request)
         self.set_status(204)
         self.finish()
