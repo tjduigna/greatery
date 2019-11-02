6a92bf487b3f349b31308750e734973c3b443583
tjduigna <tjduigna@buffalo.edu>
2019-11-02 15:15:30 -0400

    tidied buildspec

--- pipeline/userdata.sh
+++ pipeline/userdata.sh
@@ -4,7 +4,5 @@ sudo yum update -y
 sudo yum install git python36 docker -y
 sudo service docker start
 
-git clone https://github.com/tjduigna/sprout.git
-git clone https://github.com/tjduigna/onion.git
-git clone https://github.com/tjduigna/greatery.git
+git clone --recurse-submodules https://github.com/tjduigna/greatery.git
 
